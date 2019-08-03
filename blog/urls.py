51e52cc916d0e4a6a4655a2e987d54135184af31
batTrung <trungbat.pv@gmail.com>
2019-08-03 16:35:14 +0700

    add

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('basic-upload/', views.BasicUploadView.as_view(), name='basic_upload'),
+	path('', views.BasicUploadView.as_view(), name='basic_upload'),
 	path('user-upload/', views.user_upload, name='user_upload'),
 ]
