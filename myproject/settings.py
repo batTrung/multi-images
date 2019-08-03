518320f0fd685412f33dd3263344cb2f78ee3b64
batTrung <trungbat.pv@gmail.com>
2019-08-03 16:33:12 +0700

    add

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+k+6yugna&to9cs=bzfc$szx*bas&%@)&%2qmgr_-r1qo--bov'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
